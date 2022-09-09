5c7d92eea7e063f887fefcd0ecef77867ba45a8c
kdadeniji <darmeygzy@yahoo.com>
2022-09-09 10:32:58 -0700

    Task 4 Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,21 @@
 /**
- * main -entry point
  *
- * Return: 0 after printing the function
- */
+ *  * main -entry point
+ *
+ *   *
+ *
+ *    * Return: 0 after printing the function
+ *
+ *     */
+
 #include <stdio.h>
+
 int main(void)
+
 {
-       puts("\"programming is like building a multilingual puzzle");
-          return (0);
-}   	  
+
+			puts("\"Programming is like building a multilingual puzzle");
+
+						return (0);
+
+}
