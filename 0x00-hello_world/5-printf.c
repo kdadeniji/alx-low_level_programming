fa3f2699ed44ced50a3e6f85595bf300683c03e6
kdadeniji <darmeygzy@yahoo.com>
2022-09-10 05:01:53 -0700

    Task 5 Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#iclude <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that printsusing printf
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+    printf("with proper grammer, but the outcome is a piece of art,\n");
+    return (0);
+}
