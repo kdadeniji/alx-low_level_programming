381554a1c21af5402b02e4e48a87b0b3d680ba17
kdadeniji <darmeygzy@yahoo.com>
2022-09-12 16:06:02 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,14 +9,13 @@
  * this program prints "programming is positive, zero, or negative
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
         int n;
         
 	srand(time(0));
         n = rand() - RAND_MAX / 2;
-	/*my code*/
+	/* my code */
        	if (n > 0)
 	{	
 		printf("%d is positive\n", n);
