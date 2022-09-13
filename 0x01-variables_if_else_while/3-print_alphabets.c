2110d844246bc99f43bf30ebc01b8dba94135ba1
kdadeniji <darmeygzy@yahoo.com>
2022-09-13 06:45:08 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ *
+ *  * main - Program Entry point
+ *
+ *   *
+ *
+ *    * Return: 0 Always (Success)
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		char upplow;
+
+
+
+			for (upplow = 'a'; upplow <= 'z'; upplow++)
+
+						putchar(upplow);
+
+				for (upplow = 'A'; upplow <= 'Z'; upplow++)
+
+							putchar(upplow);
+
+					putchar('\n');
+
+
+
+						return (0);
+
+}
