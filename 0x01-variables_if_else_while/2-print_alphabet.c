749ef96278e1accb343a9f1a95c845aa57d4a516
kdadeniji <darmeygzy@yahoo.com>
2022-09-13 04:36:47 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: 0 Always (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char low;
+
+
+
+			for (low = 'a'; low <= 'z'; low++)
+
+						putchar(low);
+
+				putchar('\n');
+
+					return (0);
+}
