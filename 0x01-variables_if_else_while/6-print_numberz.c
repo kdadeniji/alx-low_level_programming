2ac0887721f9a104fa69b5bba72b040d3271d826
kdadeniji <darmeygzy@yahoo.com>
2022-09-13 08:14:47 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program entry
+ *
+ * Return: 0 Alway (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
