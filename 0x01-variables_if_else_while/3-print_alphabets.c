5de64ca698e46f45cd1ff7d1ad33cad54f2d6c1c
kdadeniji <darmeygzy@yahoo.com>
2022-09-13 07:17:45 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,41 +1,24 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
 /**
+ * main - Program Entry point
  *
- *  * main - Program Entry point
- *
- *   *
- *
- *    * Return: 0 Always (Success)
- *
- *     */
-
-
+ * Return: 0 Always (Success)
+ */
 
 int main(void)
 
 {
+	char upplow;
 
-		char upplow;
+	for (upplow = 'a'; upplow <= 'z'; upplow++)
+		putchar(upplow);
+	for (upplow = 'A'; upplow <= 'Z'; upplow++)
+		putchar(upplow);
+	putchar('\n');
 
-
-
-			for (upplow = 'a'; upplow <= 'z'; upplow++)
-
-						putchar(upplow);
-
-				for (upplow = 'A'; upplow <= 'Z'; upplow++)
-
-							putchar(upplow);
-
-					putchar('\n');
-
-
-
-						return (0);
-
+	return (0);
 }
+
